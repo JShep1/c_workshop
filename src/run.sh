03202459132e0b61cab249f29ba92ab07d351d66
John <johnshepherd96@yahoo.com>
2017-10-13 19:30:50 -0700

    Added ll example

--- src/run.sh
+++ src/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-gcc -g -o assert_test assert_test.c
-gcc -g -o gdb_test1 gdb_test1.c
-gcc -g -o gdb_test2 gdb_test2.c
-gcc -g -o valgrind_test1 valgrind_test1.c
-gcc -g -o valgrind_test2 valgrind_test2.c
+gcc -g -Wextra -Wall -Werror -pedantic -o assert_test assert_test.c
+gcc -g -Wextra -Wall -Werror -pedantic -o gdb_test1 gdb_test1.c
+gcc -g -Wextra -Wall -Werror -pedantic -o gdb_test2 gdb_test2.c
+gcc -g -Wextra -Wall -Werror -pedantic -o valgrind_test1 valgrind_test1.c
+gcc -g -Wextra -Wall -Werror -pedantic -o valgrind_test2 valgrind_test2.c
 
---
+++ src/runll.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gcc -g -Wextra -Wall -Werror -pedantic -o ll ll.c
+
